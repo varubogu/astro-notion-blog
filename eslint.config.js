5e8bb492990849e15518c432a97dba9ba8d1ec0f
varubogu <varubogu@users.noreply.github.com>
2024-12-22 04:01:47 +0000

    Format by GitHub Actions

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,42 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import typescriptEslintParser from "@typescript-eslint/parser";
-import pluginReact from "eslint-plugin-react";
-import eslintPluginAstro from "eslint-plugin-astro";
-import astroParser from "astro-eslint-parser";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import typescriptEslintParser from '@typescript-eslint/parser';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import astroParser from 'astro-eslint-parser';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ["**/*.{jsx,tsx}"],
+    files: ['**/*.{jsx,tsx}'],
     plugins: {
-      react: pluginReact
+      react: pluginReact,
     },
     settings: {
       react: {
-        version: "detect"
-      }
-    }
+        version: 'detect',
+      },
+    },
   },
   ...eslintPluginAstro.configs.recommended,
   {
-    files: ["**/*.astro"],
+    files: ['**/*.astro'],
     languageOptions: {
       parser: astroParser,
       parserOptions: {
         parser: typescriptEslintParser,
-        extraFileExtensions: [".astro"]
-      }
+        extraFileExtensions: ['.astro'],
+      },
     },
     rules: {
-      "astro/no-set-html-directive": "error",
-      "astro/no-set-text-directive": "error",
-      "astro/no-unused-css-selector": "warn",
-    }
-  }
+      'astro/no-set-html-directive': 'error',
+      'astro/no-set-text-directive': 'error',
+      'astro/no-unused-css-selector': 'warn',
+    },
+  },
 ];
