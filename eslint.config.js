019ebba4445fa8700bae26f3f016218138a9d37e
ヴァルボーグ@toyosuke <toyosuke@varubogu.com>
2024-12-22 03:36:49 +0000

    eslint最新版に合わせて設定ファイルを変更

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginAstro from "eslint-plugin-astro";
+import astroParser from "astro-eslint-parser";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  },
+  ...eslintPluginAstro.configs.recommended,
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: typescriptEslintParser,
+        extraFileExtensions: [".astro"]
+      }
+    },
+    rules: {
+      "astro/no-set-html-directive": "error",
+      "astro/no-set-text-directive": "error",
+      "astro/no-unused-css-selector": "warn",
+    }
+  }
+];
